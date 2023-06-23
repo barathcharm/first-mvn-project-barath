5f32cbe159a3fdddf4be6f8ed8494ed16cf79dc9
Barath <barathdhilip713@gmail.com>
2023-06-23 10:48:52 +0530

    updated

---
+++ src/main/java/com/fssa/barath/day17/solved/HelloWorldProgram.java
@@ -0,0 +1,11 @@
+package com.fssa.barath.day17.solved;
+
+public class HelloWorldProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
