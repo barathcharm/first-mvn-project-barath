6a64486fefbe09d26f8785a6a97060127ee5d833
Barath <barathdhilip713@gmail.com>
2023-06-23 10:18:15 +0530

    Added the src and pom

---
+++ src/main/java/com/fssa/barath/sampleprogs/HelloWorldProgram.java
@@ -0,0 +1,11 @@
+package com.fssa.barath.sampleprogs;
+
+public class HelloWorldProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
